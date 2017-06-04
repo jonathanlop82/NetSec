455a74b976416a1712f20cdb87add96cb77ddaf7
jonathanlop82 <jonathanlop82@gmail.com>
2017-06-04 00:16:56 -0500

    Update files

--- ejescaneo.py
+++
@@ -1,7 +0,0 @@
-import socket
-s = socket.socket()
-s.connect(('192.168.2.2', 22))
-s.send('Primal Security n')
-
-banner = s.recv(1024)
-print banner
--- ventana1.py
+++
@@ -1,9 +0,0 @@
-import Tkinter
-
-root = Tkinter.Tk()
-boton = Tkinter.Button(root, text="Minimizar", command=root.iconify)
-root.geometry("300x300")
-root.maxsize(400, 600)
-
-boton.pack()
-root.mainloop()
